2c32b18204815d4e8beb882eadb75d82618a1b2f
Lucas de Lima <lms.souza39@gmail.com>
2021-05-11 10:36:55 -0300

    :tada: update routes :tada:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import SignUpOptions from '../pages/SignUpOptions';
-=======
+
 import SignUp from '../pages/SignUp';
->>>>>>> 21a226afe7b4210a2310a67de320fb28e1f9e493
-=======
-import SignUp from '../pages/SignUp';
->>>>>>> 21a226afe7b4210a2310a67de320fb28e1f9e493
 
 const Auth = createStackNavigator();
 
@@ -23,15 +17,8 @@ const AuthRoutes: React.FC = () => (
         initialRouteName="Signin"
     >
         <Auth.Screen name="SignIn" component={SignIn} />
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Auth.Screen name="SignUpOptions" component={SignUpOptions} />
-=======
         <Auth.Screen name="SignUp" component={SignUp} />
->>>>>>> 21a226afe7b4210a2310a67de320fb28e1f9e493
-=======
-        <Auth.Screen name="SignUp" component={SignUp} />
->>>>>>> 21a226afe7b4210a2310a67de320fb28e1f9e493
     </Auth.Navigator>
 );
 
