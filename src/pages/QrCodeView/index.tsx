f3a2890cdf931dade0c032fe543dedb4f04e0474
caiovictors <cvsarmento@hotmail.com>
2021-06-16 14:55:21 -0300

    Correção qr code view

---
+++ src/pages/QrCodeView/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const QrCodeView = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>Tela do QR Code aqui</Text>
+        </View>
+    );
+}
+
+export default QrCodeView
